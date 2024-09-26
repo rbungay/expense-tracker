3f87ecef700ab6be867460839e70230e0438bbf7
Rani Bungay <ranibungay@gmail.com>
2024-09-26 18:14:10 -0400

    added mongodb connection

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
+
+mongoose.connect(process.env.MONDODB_URI);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB ", mongoose.connection.name);
+});
+
+app.get("/", async (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
