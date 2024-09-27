13877dec32e0915b8a59e232c21de65cfe8eddef
Rani Bungay <ranibungay@gmail.com>
2024-09-27 13:01:52 -0400

    functioning mongodb

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import Tracker from "./models/tracker.js";
 dotenv.config();
 const app = express();
+app.use(express.urlencoded({ extended: false }));
 
 mongoose.connect(process.env.MONDODB_URI);
 
+//connecting do backend server
 mongoose.connection.on("connected", () => {
   console.log("Connected to MongoDB ", mongoose.connection.name);
 });
@@ -15,9 +18,16 @@ app.get("/", async (req, res) => {
   res.render("index.ejs");
 });
 
-//this renders when someone wants to create a new expense
+//this renders when someone wants to create a new expense and post expense
 app.get("/newexpense", (req, res) => {
   res.render("trackers/newexpense.ejs");
+});
+
+app.post("/newexpense", async (req, res) => {
+  console.log(req.body);
+  await Tracker.create(req.body);
+
+  res.redirect("/newexpense");
 });
 
 //this renders when someone wants to view all expenses
